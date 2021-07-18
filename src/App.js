d53fb1ede8ada6e68669e256b0ff4d0b6e22758e
beanxx <tnqls991211@naver.com>
2021-07-19 06:35:52 +0900

    feat: 입력 값을 버킷리스트 아이템으로 추가 기능 input 값을 state 관리함으로써 아이템에 추가

--- src/App.js
+++ src/App.js
@@ -1,33 +1,81 @@
 import React from "react";
+// BucketList 컴포넌트를 import 해옵니다.
+// import [컴포넌트 명] from [컴포넌트가 있는 파일경로];
 import BucketList from "./BucketList";
-//import "./style.css";
-import "./scss_ex.scss";
+import styled from "styled-components";
 
+// 클래스형 컴포넌트는 이렇게 생겼습니다!
 class App extends React.Component {
   constructor(props) {
     super(props);
-    // App 컴포넌트의 state 정의
+    // App 컴포넌트의 state를 정의해줍니다.
     this.state = {
-      list: ["여행 가기", "매일 책읽기", "기타 배우기", "드라마 정주행하기"],
+      list: ["영화관 가기", "매일 책읽기", "수영 배우기"],
     };
+
+    this.text = React.createRef();
   }
 
-  // render() 안에 리액트 엘리먼트를 넣어줌.
-  render() {
-    // this 키워드를 통해 state에 접근
-    console.log(this.state);
+  addBucketList = () => {
+    let list = this.state.list;
+    const new_item = this.text.current.value;
 
+    this.setState({ list: [...list, new_item] });
+  };
+
+  componentDidMount() {
+    console.log(this.text);
+    console.log(this.text.current);
+  }
+
+  // 랜더 함수 안에 리액트 엘리먼트를 넣어줍니다!
+  render() {
     return (
       <div className="App">
-        <div className="container">
-          <h1 className="title">내 버킷리스트</h1>
-          <hr className="line" />
+        <Container>
+          <Title>내 버킷리스트</Title>
+          <Line />
+          {/* 컴포넌트를 넣어줍니다. */}
           {/* <컴포넌트 명 [props 명]={넘겨줄 것(리스트, 문자열, 숫자, ...)}/> */}
           <BucketList list={this.state.list} />
-        </div>
+        </Container>
+        <Input>
+          <input type="text" ref={this.text} />
+          <button onClick={this.addBucketList}>추가하기</button>
+        </Input>
       </div>
     );
   }
 }
+
+const Input = styled.div`
+  max-width: 350px;
+  min-height: 10vh;
+  background-color: #fff;
+  padding: 16px;
+  margin: 20px auto;
+  border-radius: 5px;
+  border: 1px solid #ddd;
+`;
+
+const Container = styled.div`
+  max-width: 350px;
+  min-height: 80vh;
+  background-color: #fff;
+  padding: 16px;
+  margin: 20px auto;
+  border-radius: 5px;
+  border: 1px solid #ddd;
+`;
+
+const Title = styled.h1`
+  color: slateblue;
+  text-align: center;
+`;
+
+const Line = styled.hr`
+  margin: 16px 0px;
+  border: 1px dotted #ddd;
+`;
 
 export default App;
--- src/BucketList.js
+++ src/BucketList.js
@@ -1,35 +1,30 @@
 import React from "react";
+import styled from "styled-components";
 
-// 함수형 컴포넌트 방법 1.
-// function Bucketlist(props){
-//     return (
-//         <div>버킷 리스트</div>
-//     );
-// }
-
-// 방법 2. =>가 들어간 함수: 화살표 함수
-// () 안에 props: 부모 컴포넌트에게 받아온 데이터
 const BucketList = (props) => {
-  // Quiz 1: my_list에 ['a', 'b', 'c'] 대신 부모 컴포넌트가 넘겨준 값을 넣으려면 어떻게 해야하나
   const my_lists = props.list;
 
-  console.log(props);
-  // 컴포넌트가 뿌려줄 ui 요소(리엑트 엘리먼트라고 불러요.)를 반환
   return (
-    <div className="lists">
-      {
-        // js의 내장 함수 중 하나인 map. 리스트의 갯수만큼 => 오른쪽 구문 반복.
-        my_lists.map((list, index) => {
-          console.log(list);
-          return (
-            <div className="list-item" key={index}>
-              {list}
-            </div>
-          );
-        })
-      }
-    </div>
+    <ListStyle>
+      {my_lists.map((list, index) => {
+        return <ItemStyle key={index}>{list}</ItemStyle>;
+      })}
+    </ListStyle>
   );
 };
+
+const ListStyle = styled.div`
+  display: flex;
+  flex-direction: column;
+  height: 100%;
+  overflow-x: hidden;
+  overflow-y: auto;
+`;
+
+const ItemStyle = styled.div`
+  padding: 16px;
+  margin: 8px;
+  background-color: aliceblue;
+`;
 
 export default BucketList;
